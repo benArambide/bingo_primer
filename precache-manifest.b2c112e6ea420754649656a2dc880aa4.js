f924c8267f882220cb6e02a907c57bab883530d5
Josue Arambide <jarembideq@proveedor.intercorp.com.pe>
2020-07-07 02:39:28 -0500

    Updates

---
+++ precache-manifest.b2c112e6ea420754649656a2dc880aa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "590b3e39c71ddbc7d1302ae35a54b279",
+    "url": "/bingo_primer/index.html"
+  },
+  {
+    "revision": "4568d897b70f1642d862",
+    "url": "/bingo_primer/static/css/main.37dbc763.chunk.css"
+  },
+  {
+    "revision": "295d7a60f8dc0eaf1df3",
+    "url": "/bingo_primer/static/js/2.6fb43641.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bingo_primer/static/js/2.6fb43641.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4568d897b70f1642d862",
+    "url": "/bingo_primer/static/js/main.608f02c3.chunk.js"
+  },
+  {
+    "revision": "426fcbe932115678967f",
+    "url": "/bingo_primer/static/js/runtime-main.fead20c8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.db08ec6c1563af1fa7cd197811668be0.js"
+  "/bingo_primer/precache-manifest.b2c112e6ea420754649656a2dc880aa4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bingo_primer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
